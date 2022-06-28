25ab7c7c1711f024482c92f1748951261add25fb
Filipe Ferreira <frferreira9@gmail.com>
2022-06-28 16:00:07 -0700

    Update Tower.gd

--- Tower.gd
+++ Tower.gd
@@ -5,7 +5,8 @@ const BULLET = preload("res://Bullet.tscn")
 const TOWER_SIZE = Vector2(25,25)
 const TOWER_POS = TOWER_SIZE/2 - TOWER_SIZE
 const TOWER_UNPLACED_COLOR = Color(1,1,1,0.5)
-const TOWER_PLACED_COLOR = Color(1,1,1)
+const TOWER_PLAYER_COLOR = Color(0,0,1)
+const TOWER_ENEMY_COLOR = Color(1,0,0)
 const TOWER_OUTLINE_SIZE = Vector2(29,29)
 const TOWER_OUTLINE_POS = TOWER_OUTLINE_SIZE/2 - TOWER_OUTLINE_SIZE
 const TOWER_OUTLINE_UNPLACED_COLOR = Color(0,0,0,0.5)
@@ -35,7 +36,7 @@ func _ready():
 func _draw():
 	if placed:
 		draw_rect(Rect2(TOWER_OUTLINE_POS, TOWER_OUTLINE_SIZE), TOWER_OUTLINE_PLACED_COLOR)
-		draw_rect(Rect2(TOWER_POS, TOWER_SIZE), TOWER_PLACED_COLOR)
+		draw_rect(Rect2(TOWER_POS, TOWER_SIZE), TOWER_PLAYER_COLOR)
 	else:
 		draw_circle(Vector2(0,0), attack_range, RANGE_OUTLINE_COLOR)
 		draw_rect(Rect2(TOWER_OUTLINE_POS, TOWER_OUTLINE_SIZE), TOWER_OUTLINE_UNPLACED_COLOR)
